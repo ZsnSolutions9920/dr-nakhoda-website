482dce5a689aa5b462d8e04d81f995b58428241a
ZsnSolutions9920 <zsnsolutions9920@gmail.com>
2026-03-10 21:00:55 +0500

    test action

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: NextRequest) {
     const { email, password } = await req.json();
 
     if (!email || !password) {
-      return NextResponse.json({ error: "Email and password are required" }, { status: 400 });
+      return NextResponse.json({ error: "Email and password are required*" }, { status: 400 });
     }
 
     const admin = await Admin.findOne({ email });
